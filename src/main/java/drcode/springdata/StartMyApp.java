b3478abd90e05f11e4bf7657b456772e81967765
Daniel <danielribas1105@gmail.com>
2023-04-19 12:02:33 -0300

    update spring project

--- src/main/java/drcode/springdata/StartMyApp.java
+++ src/main/java/drcode/springdata/StartMyApp.java
@@ -6,17 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StartMyApp implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
     @Override
     public void run(String... args) throws Exception {
+        List<User> users = userRepository.filtrarPorNome("Daniel");
+        //List<User> users = userRepository.findByNameContaining("Daniel");
+        for(User u: users){
+            System.out.println(u);
+        }
+    }
+    private void insertUser(){
         User user = new User();
         user.setName("Daniel Ribas");
         user.setNameUser("dribas");
         user.setPasswordUser("drs123");
-
         userRepository.save(user);
 
         for (User u: userRepository.findAll()) {
--- src/main/java/drcode/springdata/repository/UserRepository.java
+++ src/main/java/drcode/springdata/repository/UserRepository.java
@@ -2,6 +2,20 @@ package drcode.springdata.repository;
 
 import drcode.springdata.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
+
+    //Query Method
+    List<User> findByNameContaining(String name);
+
+    //Query Method
+    User findByNameUser(String name);
+
+    //Query Override
+    @Query("SELECT u FROM tab_user u WHERE u.name LIKE %:name%")
+    List<User> filtrarPorNome(@Param("name") String name);
 }
